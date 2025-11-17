27a24ccd1f7695bc2e288a9c8ddaebeb80f6ff69
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-11-17 20:14:33 +0530

    Increment Decrement

---
+++ Task 1/script.js
@@ -0,0 +1,13 @@
+var h1 = document.querySelector('h1')
+var inc = document.querySelector('#inc')
+var dec = document.querySelector('#dec')
+let a = 0
+
+inc.addEventListener('click',function(){
+    a++ 
+    h1.innerHTML =a
+})
+dec.addEventListener('click',function(){
+    a--
+    h1.innerHTML =a
+})

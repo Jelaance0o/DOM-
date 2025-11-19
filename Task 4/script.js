2602762902966073a31d1923ae333e03761027d4
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-11-19 22:58:06 +0530

    basics

---
+++ Task 4/script.js
@@ -0,0 +1,27 @@
+// var  btn = document.querySelector('button')
+
+// btn.addEventListener('click',function(){
+    //     var h1 = document.createElement('h1')
+    //     h1.innerHTML = 'Hello '
+    //     console.log(h1)
+    
+    // })
+   
+
+    // var h1 = document.createElement('h1')
+
+    // h1.innerHTML='hello from js'
+
+    // var main = document.querySelector('main')
+
+    // main.appendChild(h1)
+
+
+var div = document.createElement('div')
+var main = document.querySelector('main')
+
+div.style.height='200px'
+div.style.width='200px'
+div.style.backgroundColor='red'
+
+main.appendChild(div)

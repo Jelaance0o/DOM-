5266f7d4eb4fb5ed98a85a73de8878c894568cfb
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-11-20 23:21:43 +0530

    Start

--- Task 4/script.js
+++ Task 4/script.js
@@ -1,27 +1,7 @@
-// var  btn = document.querySelector('button')
-
-// btn.addEventListener('click',function(){
-    //     var h1 = document.createElement('h1')
-    //     h1.innerHTML = 'Hello '
-    //     console.log(h1)
-    
-    // })
-   
-
-    // var h1 = document.createElement('h1')
-
-    // h1.innerHTML='hello from js'
-
-    // var main = document.querySelector('main')
-
-    // main.appendChild(h1)
-
-
-var div = document.createElement('div')
+var btn = document.querySelector('button')
 var main = document.querySelector('main')
 
-div.style.height='200px'
-div.style.width='200px'
-div.style.backgroundColor='red'
+btn.addEventListener('click',function(){
 
-main.appendChild(div)
+
+})

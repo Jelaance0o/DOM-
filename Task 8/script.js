31d34c273ed8f26e13f7748c08196c0f1670e122
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-11-29 20:44:01 +0530

    Incrementr decrementr

--- Task 8/script.js
+++ Task 8/script.js
@@ -1,6 +1,17 @@
-var div = document.querySelector('div')
+// var div = document.querySelector('div')
+
+// document.addEventListener('keydown',function(det){
+//     console.log(det.key)
+//     div.innerHTML = det.key
+// })
+
+var one = new Audio('./50.mp3');
+ var cont = document.querySelector('#ctn')
 
 document.addEventListener('keydown',function(det){
     console.log(det.key)
-    div.innerHTML = det.key
+    var a
+    // if(det.key === a){
+    //     one.play()
+    // }
 })
